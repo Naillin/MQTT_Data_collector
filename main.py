7d8b9a3e130b2217efbc29ab1e9c11fa7b40e0ee
Naillin <kamilasd@yandex.ru>
2024-11-19 03:33:22 +0500

    changed journal_mode to WAL

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ import paho.mqtt.client as mqtt
 import sqlite3
 import time
 
-# Подключение к базе данных
-conn = sqlite3.connect('mqtt_data.db')
-cursor = conn.cursor()
-
-
 # Чтение списка топиков из базы
 def get_topics():
-    cursor.execute("SELECT ID_Topic, Path_Topic FROM Topics")
-    return cursor.fetchall()
-
+    # Открытие соединения и курсора для чтения
+    with sqlite3.connect('mqtt_data.db') as conn:
+        conn.execute('PRAGMA journal_mode=WAL')
+        cursor = conn.cursor()
+        cursor.execute("SELECT ID_Topic, Path_Topic FROM Topics")
+        return cursor.fetchall()
 
 # Функция для обработки входящих сообщений
 def on_message(client, userdata, message):
@@ -19,33 +17,38 @@ def on_message(client, userdata, message):
     value = message.payload.decode()
     timestamp = int(time.time())
 
-    # Найти ID_Topic по Path_Topic
-    cursor.execute("SELECT ID_Topic FROM Topics WHERE Path_Topic = ?", (topic_path,))
-    topic_row = cursor.fetchone()
+    # Открытие соединения для записи данных в базу
+    with sqlite3.connect('mqtt_data.db') as conn:
+        conn.execute('PRAGMA journal_mode=WAL')
+        cursor = conn.cursor()
 
-    if topic_row:
-        id_topic = topic_row[0]
-        # Сохранить данные в таблицу Data
-        cursor.execute("INSERT INTO Data (ID_Topic, Value_Data, Time_Data) VALUES (?, ?, ?)",
-                       (id_topic, value, timestamp))
-        conn.commit()
-        print(f"Сохранены данные: Топик {topic_path}, Значение {value}, Время {timestamp}")
+        # Найти ID_Topic по Path_Topic
+        cursor.execute("SELECT ID_Topic FROM Topics WHERE Path_Topic = ?", (topic_path,))
+        topic_row = cursor.fetchone()
 
+        if topic_row:
+            id_topic = topic_row[0]
+            # Сохранить данные в таблицу Data
+            cursor.execute("INSERT INTO Data (ID_Topic, Value_Data, Time_Data) VALUES (?, ?, ?)",
+                           (id_topic, value, timestamp))
+            conn.commit()
+            print(f"Сохранены данные: Топик {topic_path}, Значение {value}, Время {timestamp}")
 
 # Инициализация MQTT клиента
 client = mqtt.Client(protocol=mqtt.MQTTv311)
 client.on_message = on_message
 client.connect("localhost", 3121)
 
-
 # Обновление подписок
 def update_subscriptions():
-    client.unsubscribe("#")  # Отписаться от всех топиков
-    topics = get_topics()
-    for id_topic, path_topic in topics:
-        client.subscribe(path_topic)
-        print(f"Подписка на {path_topic}")
-
+    # Открытие соединения и курсора для чтения
+    with sqlite3.connect('mqtt_data.db') as conn:
+        cursor = conn.cursor()
+        client.unsubscribe("#")  # Отписаться от всех топиков
+        topics = get_topics()  # Получить список топиков
+        for id_topic, path_topic in topics:
+            client.subscribe(path_topic)
+            print(f"Подписка на {path_topic}")
 
 # Основной цикл
 try:
@@ -56,4 +59,3 @@ try:
 except KeyboardInterrupt:
     print("Отключение клиента")
     client.disconnect()
-    conn.close()
