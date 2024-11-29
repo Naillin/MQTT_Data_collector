0d4f757ea18938f3d6093e571c9d98ce4eb8cb0e
Naillin <kamilasd@yandex.ru>
2024-11-29 05:43:54 +0500

    add altitude to Topics

--- mqtt_data.sql
+++ mqtt_data.sql
@@ -1,10 +1,18 @@
+-- Создаем таблицу Users
+CREATE TABLE Users (
+    ID_User INTEGER PRIMARY KEY,
+    Login_User TEXT,
+    Password_User TEXT
+);
+
 -- Создаем таблицу Topics
 CREATE TABLE Topics (
     ID_Topic INTEGER PRIMARY KEY,
     Name_Topic TEXT NOT NULL,
     Path_Topic TEXT NOT NULL,
     Latitude_Topic REAL,
-    Longitude_Topic REAL
+    Longitude_Topic REAL,
+	Altitude_Topic REAL
 );
 
 -- Создаем таблицу Data с каскадным удалением
