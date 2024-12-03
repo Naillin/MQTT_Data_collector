fe828be454551417426a9f11f60141aeda7d2531
Naillin <kamilasd@yandex.ru>
2024-12-03 16:00:55 +0500

    add area table

--- mqtt_data.sql
+++ mqtt_data.sql
@@ -1,8 +1,8 @@
 -- Создаем таблицу Users
 CREATE TABLE Users (
     ID_User INTEGER PRIMARY KEY,
-    Login_User TEXT,
-    Password_User TEXT
+    Login_User TEXT NOT NULL,
+    Password_User TEXT NOT NULL
 );
 
 -- Создаем таблицу Topics
@@ -10,9 +10,10 @@ CREATE TABLE Topics (
     ID_Topic INTEGER PRIMARY KEY,
     Name_Topic TEXT NOT NULL,
     Path_Topic TEXT NOT NULL,
-    Latitude_Topic REAL,
-    Longitude_Topic REAL,
-	Altitude_Topic REAL
+    Latitude_Topic REAL NOT NULL,
+    Longitude_Topic REAL NOT NULL,
+	Altitude_Topic REAL NOT NULL,
+	CheckTime_Topic DATETIME
 );
 
 -- Создаем таблицу Data с каскадным удалением
@@ -22,4 +23,15 @@ CREATE TABLE Data (
     Value_Data TEXT,
     Time_Data DATETIME NOT NULL,
     FOREIGN KEY (ID_Topic) REFERENCES Topics(ID_Topic) ON DELETE CASCADE
+);
+
+-- Создаем таблицу AreaPoints с каскадным удалением
+CREATE TABLE AreaPoints (
+    ID_AreaPoint INTEGER PRIMARY KEY,
+    ID_Topic INTEGER NOT NULL,
+    Depression_AreaPoint TEXT NOT NULL,
+    Perimeter_AreaPoint TEXT NOT NULL,
+    Included_AreaPoint TEXT NOT NULL,
+    Islands_AreaPoint TEXT NOT NULL,
+    FOREIGN KEY (ID_Topic) REFERENCES Topic(ID_Topic) ON DELETE CASCADE
 );
